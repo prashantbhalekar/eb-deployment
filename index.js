d80df7cff4d0304be9d1b3e27a3f1f8e00836477
Prashant Bhalekar <prashantbhalekar474@gmail.com>
2025-07-12 14:49:20 +0530

    changed ui heading

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express(express.json());
 
 app.get("", (req, res) => {
-  res.send("Deployment With Docker!");
+  res.send("Deployment With Docker and github action!");
 });
 
 app.get("/json", (req, res) => {
